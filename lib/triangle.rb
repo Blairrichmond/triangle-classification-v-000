72fbfd44d2c09bc59fa89f131411fe7c785d76d9
Brent  Ransom <sadumedusa@gmail.com>
2018-02-27 03:54:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,12 @@
 class Triangle
 
   attr_accessor :right_Side, :left_side, :bottom_side
+  attr_reader :sorted
 
   def initialize(right_Side, left_side, bottom_side)
-
+    @right_Side = right_Side
+    @left_side = left_side
+    @bottom_side = bottom_side
+    @sorted = [@right_Side, @left_side, @bottom_side].sort
   end
 end
